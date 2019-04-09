dbd512d52688b3c3ff0d91cfbd77f01365df8016
ian8924 <justforian0915@gmail.com>
2019-04-09 16:02:54 +0800

    4.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+app.use(express.static('public'));
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+        list: require('./../helpers/list.js')
+    }
+}));
+
+app.set('view engine', 'hbs');
 
 // routes
 app.get('/', (req, res)=>{
-    res.send('Hello Express');
+    const sales = require('./../data/sales.json');
+    res.render('home', {
+        // layout: false,
+        name: sales[0].name,
+    });
 });
 
-app.get('/abc', (req, res)=>{
+app.get('/sales', (req, res)=>{
+    const sales = require('./../data/sales.json');
+    res.render('sales', {
+        // layout: false,
+        sales: sales
+    });
+});
+
+
+app.get('/sales2', (req, res)=>{
+        const sales = require('./../data/sales.json');    
+        res.render('sales2', {        
+            sales: sales    
+        });
+    });
+
+app.get('/abc.html', (req, res)=>{
     res.send('ABC');
 });
 
