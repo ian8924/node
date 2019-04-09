1445172ca860ca4c0422f6f45785d4e58b97c7f7
ian8924 <justforian0915@gmail.com>
2019-04-09 16:51:05 +0800

    last

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const url = require('url');
+
+const bodyParser = require('body-parser');
+const bodyParserUrlencoded = bodyParser.urlencoded({extended: false});
 
 const app = express();
 app.use(express.static('public'));
@@ -21,6 +25,31 @@ app.get('/', (req, res)=>{
         // layout: false,
         name: sales[0].name,
     });
+});
+
+
+// app.get('/tr_qs', (req, res)=>{    
+//     const urlParts = url.parse(req.url, true);    
+//     // 取得 QueryString (query屬性)，第二個參數表示要解析成 Object (Null Prototype)    
+//     urlParts.myQuery = JSON.parse(JSON.stringify(urlParts.query));    
+//     console.log(urlParts);    
+//     res.render('try_querystring',{        
+//         urlParts: urlParts    
+//     });
+// });
+
+app.get('/try-qs', (req, res)=>{
+    console.log(req.url);
+    const urlParts = url.parse(req.url, true);
+    console.log(urlParts);
+    res.render('try_qs', {
+        urlParts: urlParts
+    })
+});
+
+app.post('/post-echo', bodyParserUrlencoded, (req, res)=>{
+    //res.send( JSON.stringify(req.body));
+    res.json(req.body);
 });
 
 app.get('/sales', (req, res)=>{
